9d62d8e27d12f2a8ab299247e07516c26f92273e
NEDEN249 <nathaneden483@yahoo.com.au>
2023-09-15 17:15:05 +0800

    complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let filtered = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    //everything is now lowercase and unwanted characters are removed
+    var len = filtered.length;
+    for (let i = 0; i < len/2; i++){
+        if (filtered[i]!==filtered[len-1-i]){
+            return false
+        }
+    }
+    return true;
 };
+
+const palindromes2 = function (str) {
+    let re = /[^a-z0-9]/g;
+    let filtered = str.toLowerCase().replace(re, '');
+    let reversed = filtered.split('').reverse().join('');
+    return filtered == reversed;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS";
+    }
+    let curr = 0;
+    let next = 1;
+    let total = curr + next;
+    let counter = 3;
+    while (counter <= num){
+        curr = next;
+        next = total;
+        total = curr + next;
+        counter++
+    }
+    return total;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
